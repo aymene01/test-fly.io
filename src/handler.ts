6c934775df78801d96e535502c0f303e17a9acb2
aymene01 <aymene01@hotmail.fr>
2024-07-18 23:41:54 +0200

    add health route

--- src/handler.ts
+++ src/handler.ts
@@ -1,7 +1,23 @@
-import { IncomingMessage, ServerResponse } from "http";
+import { IncomingMessage, ServerResponse } from 'http'
+import { parse } from 'node:url'
 
-export function handler(req: IncomingMessage, res: ServerResponse){
-    const { method, url } = req
-    console.log({ method, url })
+const allRoutes: {
+  [key: string]: (req: IncomingMessage, res: ServerResponse) => void | Promise<void>
+} = {
+  '/health:get': (req: IncomingMessage, res: ServerResponse) => {
     res.end('Hello World \n')
+  },
+  default: (req: IncomingMessage, res: ServerResponse) => {
+    res.end('Ouuuups not found \n')
+  },
+}
+
+export function handler(req: IncomingMessage, res: ServerResponse) {
+  const { method, url } = req
+  const { pathname } = parse(url, true)
+
+  const key = `${pathname}:${method?.toLocaleLowerCase()}`
+  const choosen = allRoutes[key] || allRoutes.default
+
+  return choosen(req, res)
 }
